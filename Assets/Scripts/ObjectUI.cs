ddbb7e0281ad4d90337fc054fcfa940084196173
aiden1614 <aiden.bugarin.carreira@gmail.com>
2025-06-16 18:45:32 +0200

    added all objects

--- Assets/Scripts/ObjectUI.cs
+++ Assets/Scripts/ObjectUI.cs
@@ -4,16 +4,25 @@ public class XRPanelSwitcher : MonoBehaviour
 {
     public GameObject panel1;
     public GameObject panel2;
+    public GameObject panel3;
+    public void ShowPanel1()
+    {
+        panel1.SetActive(true);
+        panel3.SetActive(false);
+        panel2.SetActive(false);
+    }
 
     public void ShowPanel2()
     {
         panel1.SetActive(false);
         panel2.SetActive(true);
+        panel3.SetActive(false);
     }
 
-    public void ShowPanel1()
+    public void ShowPanel3()
     {
+        panel1.SetActive(false);
         panel2.SetActive(false);
-        panel1.SetActive(true);
+        panel3.SetActive(true);
     }
 }
